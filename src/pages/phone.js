9c8584c5ebb5498716f6f7a8e59eb29c60e769d4
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-11-12 20:57:09 +0530

    Phone Fix #4

--- src/pages/phone.js
+++ src/pages/phone.js
@@ -24,80 +24,80 @@ function Phone() {
           {/*Row 1 */}
           <div className="flex flex-row flex-wrap justify-center md:justify-evenly w-screen text-yellow-400 os text-lg md:text-2xl text-center items-center border-b-2 border-white rounded-xl p-4 mt-4">
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group"
               id="block-gray"
             >
               <h1> Loops </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <FontAwesomeIcon
                 icon={faInfinity}
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               ></FontAwesomeIcon>
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Methods </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <Icon
                 icon={codeCurly}
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               />
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Strings </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <Icon
                 icon="mdi:sort-alphabetical-variant"
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               />
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Classes </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <FontAwesomeIcon
                 icon={faCode}
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               ></FontAwesomeIcon>
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Arrays </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <Icon
                 icon="bx:bracket"
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               />
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Patterns </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <Icon
                 icon="bxs:shapes"
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               />
             </div>
             <div
-              className="w-24 md:w-48 h-20 md:h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
+              className="w-24 md:w-48 h-32 shadow-lg shadow-black bg-transparent scale-90 md:hover:scale-100 relative flex flex-col items-center justify-center md:justify-start group hover:cursor-pointer"
               id="block-gray"
             >
               <h1> Series </h1>
               <h2 className="text-white md:text-2xl text-sm">Programs</h2>
               <Icon
                 icon="fluent:math-symbols-32-filled"
-                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 hidden md:block"
+                className="relative top-6 text-3xl group-hover:text-4xl transition-all ease-in-out duration-700 "
               />
             </div>
           </div>
